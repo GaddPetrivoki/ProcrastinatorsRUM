aef2ffeec840445a1b6968aa988f2b7802a970ba
joselazcano <jose.lazcano@upr.edu>
2021-04-01 14:06:24 -0400

    Adding a new field (EMAIL) to the registration form.

--- ZeitNehmer/Main_Page/views.py
+++ ZeitNehmer/Main_Page/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
@@ -13,13 +13,13 @@ def login_Page(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('Main-Page')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'Main_Page/register.html', {'form': form})
