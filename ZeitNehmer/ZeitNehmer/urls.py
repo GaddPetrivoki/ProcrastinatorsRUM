9eb4505e9eb6c58bb6a56059c05a507669d9859b
joselazcano <jose.lazcano@upr.edu>
2021-04-02 12:16:15 -0400

    Home Page Progress. fix

--- ZeitNehmer/ZeitNehmer/urls.py
+++ ZeitNehmer/ZeitNehmer/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),            #URL to 'admin' page
     path('', include('Main_Page.urls')),        #URL to the 'Main Page'
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), $
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout') #
 
 
