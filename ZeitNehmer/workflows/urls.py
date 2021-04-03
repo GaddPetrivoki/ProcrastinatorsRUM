d16e39f45b8642b0f01bbda91eeeb060881f706e
Fernando Ramos-Lugo <fernando.ramos9@upr.edu>
2021-04-03 14:56:36 -0500

    Added option to update and delete(work in progress)

--- ZeitNehmer/workflows/urls.py
+++ ZeitNehmer/workflows/urls.py
@@ -5,5 +5,8 @@ from workflows.views import WorkflowsListView
 
 urlpatterns = [
     path('', WorkflowsListView.as_view(), name='Workflows'),
-    path('create/', views.workflowsCreation, name='creation')
+    path('create/', views.workflowsCreation, name='creation'),
+    path('<int:pk>/', views.workflowSingleView, name='single'),
+    path('<int:pk>/update/', views.WorkflowUpdate.as_view(), name='update_workflow'),
+    path('<int:pk>/delete/', views.WorkflowDelete.as_view(), name='delete_workflow')
 ]
--- ZeitNehmer/workflows/views.py
+++ ZeitNehmer/workflows/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django_tables2 import SingleTableView
 from django.contrib import messages
-
+from django.views.generic.edit import UpdateView, DeleteView
 from .models import Workflows
 from .tables import WorkflowsTable
 from .forms import WorkflowsForm
 
+from django.urls import reverse_lazy
 
 class WorkflowsListView(SingleTableView):
     model = Workflows
@@ -23,3 +24,17 @@ def workflowsCreation(request):
         form = WorkflowsForm()
 
         return render(request, 'new_workflow.html', {'form': form})
+
+class WorkflowUpdate(UpdateView):
+    model = Workflows
+    fields = ["name", "description", "dueDate", "priority"]
+    template_name = "updateWorkflow.html"
+    success_url = reverse_lazy('Workflows')
+
+class WorkflowDelete(DeleteView):
+    model = Workflows
+    success_url = reverse_lazy('Workflows')
+    template_name = 'deleteWorkflow.html'
+
+def workflowSingleView(request, pk):
+    return render(request,'workflowView.html')
