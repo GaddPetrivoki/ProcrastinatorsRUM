9a99d636f54dbd3ef2da8b530dd04458d66da2b9
Fernando Ramos-Lugo <fernando.ramos9@upr.edu>
2021-04-04 12:27:26 -0500

    Change a bit of the process of updating and deleting

--- ZeitNehmer/workflows/tables.py
+++ ZeitNehmer/workflows/tables.py
@@ -1,19 +1,26 @@
 import django_tables2 as tables
 from .models import Workflows
+from django.utils.safestring import mark_safe
 
 class CustomTemplateColumn(tables.TemplateColumn):
    def render(self, record, table, value, bound_column, **kwargs):
         return super(CustomTemplateColumn, self).render(record, table, value, bound_column, **kwargs)
 
 
+
+
 class WorkflowsTable(tables.Table):
-    viewButton = '<a href="/workflows/{{record.id}}/">VIEW</a>'
+    updateButton = '<a href="/workflows/{{record.id}}/update" class="btn btn-primary">UPDATE</a>'
+    updateWorkflow = CustomTemplateColumn(updateButton)
+
+    viewButton = '<a href="/workflows/{{record.id}}/delete" class="btn btn-danger">DELETE</a>'
     viewWorkflow = CustomTemplateColumn(viewButton)
 
     dueDate = tables.DateTimeColumn(format ='M d Y')
-    
+
+
+
     class Meta:
         model = Workflows
         template_name = "django_tables2/bootstrap.html"
-
-        fields = ("name", "description", 'dueDate', "priority" )
+        fields =  ('name', 'description', 'dueDate', 'priority' )
