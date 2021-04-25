cff018599cfd49f3230400a5e1baa359ba9ab95d
joselazcano <jose.lazcano@upr.edu>
2021-04-25 18:34:27 -0400

    Created Direct app, and send_message function.

---
+++ ZeitNehmer/direct/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    body = models.TextField(max_length=1000,blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def send_message(from_user, to_user, body):
+        sender_message = Message(
+            user=from_user,
+            sender=from_user,
+            recipient=to_user,
+            body=body,
+            is_read=True
+        )
+        sender_message.save()
+
+        recipient_message = Message(
+            user=to_user,
+            sender=from_user,
+            body=body,
+            recipient=from_user
+        )
+        recipient_message.save()
+
+        return sender_message
+
